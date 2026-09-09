a8119c5d4ac4ee7f298f13b5fe68b81d7fdc1fe5
m-beau <maximebeaujeanroch047@gmail.com>
2026-09-09 00:27:13 -0400

    v1.2.0

--- pixelmap/__init__.py
+++ pixelmap/__init__.py
@@ -7,6 +7,6 @@ Includes both a programmatic API and an interactive browser-based GUI.
 
 from .backend import *
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
 __author__ = "Maxime Beau"
 __email__ = "maxime@princeton.edu"
